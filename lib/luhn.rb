923e16f9da97c2ce16116c1d95d96b6876aadac8
r3alfak3 <stefanisaia@gmail.com>
2019-09-17 23:51:21 +0000

    Tests Passed

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,20 +1,19 @@
 module Luhn
     def self.is_valid?(cc_number)
-        single_digits = cc_number.to_s.split()
-        single_digits = []
+        single_digits = cc_number.to_s.split('')
         luhn_number = single_digits.reverse
         sum, i = 0, 0
-            luhn_number.each_with_index do |x|
+            luhn_number.each_with_index do |x, i|
                 ccn    = x.to_i
                 ccn   *= 2 if i.odd?
                 ccn    = 1 + (ccn - 10) if ccn >= 10
-                sum += n
-                i   += 1
+                sum   += ccn
             end
-        if (sum % 10).zero?
-            true 
+            puts sum
+            if (sum % 10).zero?
+            return true 
         else
-            false
+            return false
         end
     end
 end
